736dc3f3cd2c73538ce715cbaa5555df4c90e9a8
dubravko <mdcajic@yahoo.com>
2021-12-16 15:56:31 +0100

    Updated code with final version for the first part of the assignment. There is no gui at the moment and there are alerts instead of the console logs. Game plays 5 rounds and it ends.

--- js/game.js
+++ js/game.js
@@ -1,19 +1,45 @@
-function computerPlay() {
-    const gameElements = ['Rock', 'Paper', 'Scissors'];
+function computerPlay() {         // this function randomly chooses one string for PC
+    const gameElements = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random()* gameElements.length)
     return(random, gameElements[random]);
 }
-computerPlay()
-console.log(computerPlay());
 
-function playSingleRound(playerSelection, computerSelection) {
-    let result = prompt('Please make your choice', '')
-    if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        return 
+function playSingleRound(playerSelection, computerSelection) {   // this function plays one round of game and returns a result in form of a string 
+    
+    if ((playerSelection === 'rock' && computerSelection === 'paper')||(playerSelection === 'paper' && computerSelection === 'scissors')||(playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return 'You lost. Go delete your existence';
+    } else if ((playerSelection === 'paper' && computerSelection === 'rock')||(playerSelection === 'rock' && computerSelection === 'scissors')||(playerSelection === 'scissors' && computerSelection === 'paper')) { 
+        return  'You won, dude you are awesome';
+    } else if (playerSelection === computerSelection) {
+        return 'it\'s a tie!';
     }
     
-    return result
 }
-const playerSelection = 'Rock';
-const computerSelection = 'computerPlay()';
-console.log(playSingleRound(playerSelection, computerSelection));
+// const playerInputSelection = prompt('Please make your choice between rock, paper and scissors', '');
+
+// this makes everything that user inputs to lower case and stores it in playerSelection varibale
+// const playerSelection = playerInputSelection.toLowerCase(); 
+
+// this stores computer random choice to variable
+// const computerSelection = computerPlay(); 
+
+// console.log("Player selected: " + playerSelection);
+// console.log("Computer selected: " + computerSelection);
+// console.log(playSingleRound(playerSelection, computerSelection));
+
+
+// function that plays 5 round game that keeps score and reports winner or loser at the end
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+    const playerInputSelection = prompt('Please make your choice between rock, paper and scissors', '');
+    const playerSelection = playerInputSelection.toLowerCase();    
+    const computerSelection = computerPlay(); 
+    playSingleRound(playerSelection, computerSelection);
+    alert('You selected: ' + playerSelection);
+    alert('Computer selected: ' + computerSelection);
+    alert(playSingleRound(playerSelection, computerSelection));
+    }
+}
+
+game();
